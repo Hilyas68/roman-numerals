7f487f2d7d66a729dfd8242756ddaa24c9b0e82d
Hilyas68 <htope68@gmail.com>
2023-08-18 16:44:03 +0100

    Added implementation and enhancement for converting to number

--- src/main/java/org/example/romannumerals/RomanNumerals.java
+++ src/main/java/org/example/romannumerals/RomanNumerals.java
@@ -1,14 +1,11 @@
 package org.example.romannumerals;
 
+import java.util.Arrays;
+
 public class RomanNumerals {
 
-    private static final String[] ROMAN_NUMERALS = {
-            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-    };
-
-    private static final int[] NUMBERS = {
-            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-    };
+    private static final String[] ROMAN_NUMERALS = {"I", "V", "X", "L", "C", "D", "M"};
+    private static final int[] NUMBERS = {1, 5, 10, 50, 100, 500, 1000};
 
 
     public String convertToNumeral(int number) {
@@ -16,19 +13,67 @@ public class RomanNumerals {
         if (number <= 0 || number >= 4000) {
             throw new IllegalArgumentException("Invalid input for Roman numeral conversion");
         }
+        switch (number) {
+            case 4:
+                return "IV";
+            case 9:
+                return "IX";
+            case 40:
+                return "XL";
+            case 90:
+                return "XC";
+            case 400:
+                return "CD";
+            case 900:
+                return "CM";
+            default:
+                StringBuilder romanNumeral = new StringBuilder();
 
-        StringBuilder romanNumeral = new StringBuilder();
+                for (int i = ROMAN_NUMERALS.length - 1; i >= 0; i--) {
+                    while (number >= NUMBERS[i]) {
+                        if (i == 0 || NUMBERS[i - 1] <= number / 2) {
+                            romanNumeral.append(ROMAN_NUMERALS[i]);
+                            number -= NUMBERS[i];
+                        } else {
+                            // If the current Roman numeral is less than half of the number, then we can't subtract it
+                            // without going below 1. In this case, we just append the Roman numeral as-is.
+                            romanNumeral.append(ROMAN_NUMERALS[i]);
+                        }
+                    }
+                }
 
-        for (int i = 0; i < NUMBERS.length; i++) {
-            while (number >= NUMBERS[i]) {
-                number -= NUMBERS[i];
-                romanNumeral.append(ROMAN_NUMERALS[i]);
-            }
+                return romanNumeral.toString();
         }
-        return romanNumeral.toString();
     }
 
     public int convertToNumber(String numeral) {
-        return 0;
+
+        if (numeral == null || numeral.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input for Roman numeral conversion");
+        }
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = numeral.length() - 1; i >= 0; i--) {
+            char currentChar = numeral.charAt(i);
+            int currentIndex = Arrays.asList(ROMAN_NUMERALS).indexOf(String.valueOf(currentChar));
+
+            if (currentIndex == -1) {
+                throw new IllegalArgumentException("Invalid input for Roman numeral conversion");
+            }
+
+            int currentValue = NUMBERS[currentIndex];
+
+            if (currentValue >= prevValue) {
+                result += currentValue;
+            } else {
+                result -= currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
     }
 }
