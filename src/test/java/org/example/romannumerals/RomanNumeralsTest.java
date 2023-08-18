3d5f0de274d00ca0a412ef4f813686eb59bf1e0c
Hilyas68 <htope68@gmail.com>
2023-08-18 15:22:57 +0100

    Added unit test for converting to roman numeral

--- src/test/java/org/example/romannumerals/RomanNumeralsTest.java
+++ src/test/java/org/example/romannumerals/RomanNumeralsTest.java
@@ -2,22 +2,47 @@ package org.example.romannumerals;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RomanNumeralsTest {
 
-  static RomanNumerals romanNumerals;
+    static RomanNumerals romanNumerals;
 
-  @BeforeAll
-  public static void beforeAll(){
-    romanNumerals = new RomanNumerals();
-  }
+    @BeforeAll
+    public static void beforeAll() {
+        romanNumerals = new RomanNumerals();
+    }
 
-  @Test
-  public void givenInt5ThenConvertReturnsV() {
-    String expected = "V";
-    String actual = romanNumerals.convertToNumeral(5);
-    assertEquals(expected, actual);
-  }
+    @ParameterizedTest
+    @CsvSource({
+            "1, I",
+            "5, V",
+            "10, X",
+            "50, L",
+            "100, C",
+            "500, D",
+            "1000, M",
+            "4, IV",
+            "9, IX",
+            "40, XL",
+            "90, XC",
+            "400, CD",
+            "900, CM"
+    })
+    public void givenNumberValueThenConvertToExpectedNumeral(int number, String romanNumeral) {
+        assertEquals(romanNumeral, romanNumerals.convertToNumeral(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1, IllegalArgumentException",
+            "4000, IllegalArgumentException"
+    })
+    public void givenInvalidNumberValueThenThrowException(int number, Class<? extends Exception> exceptionClass) {
+        assertThrows(exceptionClass, () -> romanNumerals.convertToNumeral(number));
+    }
 }
